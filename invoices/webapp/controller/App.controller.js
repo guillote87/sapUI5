0324427ffb18e1060464044792bbd05ceb9d4afc
guillote87 <guillermo27@gmail.com>
2022-06-28 19:17:43 +0000

    first commit

---
+++ invoices/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("gqdev.invoices.controller.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
